8d8ac1ff260ab69f91420746e79d668cb1ee2acc
Vy.Hong <Vy.Hong@dsit.gov.uk>
2025-02-05 22:52:22 +0000

    Add more exercises

--- chapter0_fundamentals/exercises/part0_prereqs/test.py
+++ chapter0_fundamentals/exercises/part0_prereqs/test.py
@@ -164,9 +164,6 @@ def sample_distribution(probs: Tensor, n: int) -> Tensor:
     """
     rands = t.rand(n)
     cum_probs = t.cumsum(probs, dim=0)
-
-    print(rands.shape)
-    print(cum_probs.shape)
     return t.searchsorted(cum_probs, rands)
 
 
@@ -176,3 +173,187 @@ freqs = t.bincount(sample_distribution(probs, n)) / n
 assert_all_close(freqs, probs)
 
 # %%
+# E
+
+def classifier_accuracy(scores: Tensor, true_classes: Tensor) -> Tensor:
+    """Return the fraction of inputs for which the maximum score corresponds to the true class for that input.
+
+    scores: shape (batch, n_classes). A higher score[b, i] means that the classifier thinks class i is more likely.
+    true_classes: shape (batch, ). true_classes[b] is an integer from [0...n_classes).
+
+    Use t.argmax.
+    """
+    return scores.argmax(dim=1).eq(true_classes).float().mean()
+
+
+scores = t.tensor([[0.75, 0.5, 0.25], [0.1, 0.5, 0.4], [0.1, 0.7, 0.2]])
+true_classes = t.tensor([0, 1, 0])
+expected = 2.0 / 3.0
+assert classifier_accuracy(scores, true_classes) == expected
+print("Tests passed!")
+
+#%%
+# F
+
+def total_price_indexing(prices: Tensor, items: Tensor) -> float:
+    """Given prices for each kind of item and a tensor of items purchased, return the total price.
+
+    prices: shape (k, ). prices[i] is the price of the ith item.
+    items: shape (n, ). A 1D tensor where each value is an item index from [0..k).
+
+    Use integer array indexing. The below document describes this for NumPy but it's the same in PyTorch:
+
+    https://numpy.org/doc/stable/user/basics.indexing.html#integer-array-indexing
+    """
+    res = prices[items].sum().item()
+    return res
+
+
+prices = t.tensor([0.5, 1, 1.5, 2, 2.5])
+items = t.tensor([0, 0, 1, 1, 4, 3, 2])
+assert total_price_indexing(prices, items) == 9.0
+print("Tests passed!")
+
+# %%
+
+# F2
+def gather_2d(matrix: Tensor, indexes: Tensor) -> Tensor:
+    """Perform a gather operation along the second dimension.
+
+    matrix: shape (m, n)
+    indexes: shape (m, k)
+
+    Return: shape (m, k). out[i][j] = matrix[i][indexes[i][j]]
+
+    For this problem, the test already passes and it's your job to write at least three asserts relating the arguments and the output. This is a tricky function and worth spending some time to wrap your head around its behavior.
+
+    See: https://pytorch.org/docs/stable/generated/torch.gather.html?highlight=gather#torch.gather
+    """
+    # YOUR CODE HERE - add assert statement(s) here for `indices` and `matrix`
+    assert indexes.ndim == matrix.ndim
+    assert indexes.shape[0] <= matrix.shape[0]
+
+    out = matrix.gather(1, indexes)
+    # YOUR CODE HERE - add assert statement(s) here for `out`
+    assert out.shape == indexes.shape
+
+    return out
+
+
+matrix = t.arange(15).view(3, 5)
+print(f"matrix: {matrix}")
+indexes = t.tensor([[4], [3], [2]])
+expected = t.tensor([[4], [8], [12]])
+assert_all_equal(gather_2d(matrix, indexes), expected)
+
+indexes2 = t.tensor([[2, 4], [1, 3], [0, 2]])
+expected2 = t.tensor([[2, 4], [6, 8], [10, 12]])
+assert_all_equal(gather_2d(matrix, indexes2), expected2)
+
+# %%
+# F3
+
+def total_price_gather(prices: Tensor, items: Tensor) -> float:
+    """Compute the same as total_price_indexing, but use torch.gather."""
+    assert items.max() < prices.shape[0]
+    return prices.gather(0, items).sum().item()
+
+
+prices = t.tensor([0.5, 1, 1.5, 2, 2.5])
+items = t.tensor([0, 0, 1, 1, 4, 3, 2])
+assert total_price_gather(prices, items) == 9.0
+print("Tests passed!")
+
+# %%
+# G
+
+def integer_array_indexing(matrix: Tensor, coords: Tensor) -> Tensor:
+    """Return the values at each coordinate using integer array indexing.
+
+    For details on integer array indexing, see:
+    https://numpy.org/doc/stable/user/basics.indexing.html#integer-array-indexing
+
+    matrix: shape (d_0, d_1, ..., d_n)
+    coords: shape (batch, n)
+
+    Return: (batch, )
+    """
+
+    # indices = [coords[:, i] for i in range(coords.shape[1])]
+    # res = matrix[indices]
+    # return res
+    return matrix[tuple(coords.T)]
+
+
+mat_2d = t.arange(15).view(3, 5)
+print(mat_2d)
+coords_2d = t.tensor([[0, 1], [0, 4], [1, 4]])
+actual = integer_array_indexing(mat_2d, coords_2d)
+assert_all_equal(actual, t.tensor([1, 4, 9]))
+
+mat_3d = t.arange(2 * 3 * 4).view((2, 3, 4))
+coords_3d = t.tensor([[0, 0, 0], [0, 1, 1], [0, 2, 2], [1, 0, 3], [1, 2, 0]])
+actual = integer_array_indexing(mat_3d, coords_3d)
+assert_all_equal(actual, t.tensor([0, 5, 10, 15, 20]))
+
+# %%
+
+# H1
+import math
+
+def batched_logsumexp(matrix: Tensor) -> Tensor:
+    """For each row of the matrix, compute log(sum(exp(row))) in a numerically stable way.
+
+    matrix: shape (batch, n)
+
+    Return: (batch, ). For each i, out[i] = log(sum(exp(matrix[i]))).
+
+    Do this without using PyTorch's logsumexp function.
+
+    A couple useful blogs about this function:
+    - https://leimao.github.io/blog/LogSumExp/
+    - https://gregorygundersen.com/blog/2020/02/09/log-sum-exp/
+    """
+    a = matrix.max(dim=1).values # shape (batch, )
+    return a + t.log(t.sum(t.exp(matrix - einops.rearrange(a, 'n -> n 1')), dim=1))
+
+
+matrix = t.tensor([[-1000, -1000, -1000, -1000], [1000, 1000, 1000, 1000]])
+expected = t.tensor([-1000 + math.log(4), 1000 + math.log(4)])
+actual = batched_logsumexp(matrix)
+assert_all_close(actual, expected)
+
+matrix2 = t.randn((10, 20))
+expected2 = t.logsumexp(matrix2, dim=-1)
+actual2 = batched_logsumexp(matrix2)
+assert_all_close(actual2, expected2)
+
+# %%
+# H2
+
+def batched_softmax(matrix: Tensor) -> Tensor:
+    """For each row of the matrix, compute softmax(row).
+
+    Do this without using PyTorch's softmax function.
+    Instead, use the definition of softmax: https://en.wikipedia.org/wiki/Softmax_function
+
+    matrix: shape (batch, n)
+
+    Return: (batch, n). For each i, out[i] should sum to 1.
+    """
+    raise NotImplementedError()
+
+
+matrix = t.arange(1, 6).view((1, 5)).float().log()
+expected = t.arange(1, 6).view((1, 5)) / 15.0
+actual = batched_softmax(matrix)
+assert_all_close(actual, expected)
+for i in [0.12, 3.4, -5, 6.7]:
+    assert_all_close(actual, batched_softmax(matrix + i))  # check it's translation-invariant
+
+matrix2 = t.rand((10, 20))
+actual2 = batched_softmax(matrix2)
+assert actual2.min() >= 0.0
+assert actual2.max() <= 1.0
+assert_all_equal(actual2.argsort(), matrix2.argsort())
+assert_all_close(actual2.sum(dim=-1), t.ones(matrix2.shape[:-1]))
