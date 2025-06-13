7ed4ae7f5c0b6389835a6b54c108384cd9137da6
Vy.Hong <Vy.Hong@dsit.gov.uk>
2025-06-13 21:27:32 +0000

    Do 0.5 exercises until training autoencoder

--- chapter0_fundamentals/exercises/part5_vaes_and_gans/notes.py
+++ chapter0_fundamentals/exercises/part5_vaes_and_gans/notes.py
@@ -41,6 +41,7 @@ if f"{root}/{chapter}/exercises" not in sys.path:
     sys.path.append(f"{root}/{chapter}/exercises")
 
 os.chdir(f"{root}/{chapter}/exercises")
+
 # %%
 
 import os
@@ -78,3 +79,253 @@ from plotly_utils import imshow
 
 device = t.device("mps" if t.backends.mps.is_available() else "cuda" if t.cuda.is_available() else "cpu")
 # %%
+
+celeb_data_dir = section_dir / "data/celeba"
+celeb_image_dir = celeb_data_dir / "img_align_celeba"
+
+os.makedirs(celeb_image_dir, exist_ok=True)
+
+if len(list(celeb_image_dir.glob("*.jpg"))) > 0:
+    print("Dataset already loaded.")
+else:
+    dataset = load_dataset("nielsr/CelebA-faces")
+    print("Dataset loaded.")
+
+    for idx, item in tqdm(enumerate(dataset["train"]), total=len(dataset["train"]), desc="Saving imgs...", ascii=True):
+        # The image is already a JpegImageFile, so we can directly save it
+        item["image"].save(celeb_image_dir / f"{idx:06}.jpg")
+
+    print("All images have been saved.")
+
+# %%
+
+def get_dataset(dataset: Literal["MNIST", "CELEB"], train: bool = True) -> Dataset:
+    assert dataset in ["MNIST", "CELEB"]
+
+    if dataset == "CELEB":
+        image_size = 64
+        assert train, "CelebA dataset only has a training set"
+        transform = transforms.Compose(
+            [
+                transforms.Resize(image_size),
+                transforms.CenterCrop(image_size),
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
+        trainset = datasets.ImageFolder(root=exercises_dir / "part5_vaes_and_gans/data/celeba", transform=transform)
+
+    elif dataset == "MNIST":
+        img_size = 28
+        transform = transforms.Compose(
+            [transforms.Resize(img_size), transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        )
+        trainset = datasets.MNIST(
+            root=exercises_dir / "part5_vaes_and_gans/data",
+            transform=transform,
+            download=True,
+            train=train,
+        )
+
+
+    return trainset
+# %%
+
+def display_data(x: Tensor, nrows: int, title: str):
+    """Displays a batch of data, using plotly."""
+    ncols = x.shape[0] // nrows
+    # Reshape into the right shape for plotting (make it 2D if image is monochrome)
+    y = einops.rearrange(x, "(b1 b2) c h w -> (b1 h) (b2 w) c", b1=nrows).squeeze()
+    # Normalize in the 0-1 range, then map to integer type
+    y = (y - y.min()) / (y.max() - y.min())
+    y = (y * 255).to(dtype=t.uint8)
+    # Display data
+    imshow(
+        y,
+        binary_string=(y.ndim == 2),
+        height=50 * (nrows + 4),
+        width=50 * (ncols + 5),
+        title=f"{title}<br>single input shape = {x[0].shape}",
+    )
+
+
+trainset_mnist = get_dataset("MNIST")
+trainset_celeb = get_dataset("CELEB")
+
+# %%
+# Display MNIST
+x = next(iter(DataLoader(trainset_mnist, batch_size=25)))[0]
+display_data(x, nrows=5, title="MNIST data")
+
+# Display CelebA
+x = next(iter(DataLoader(trainset_celeb, batch_size=25)))[0]
+display_data(x, nrows=5, title="CelebA data")
+
+# %%
+testset = get_dataset("MNIST", train=False)
+HOLDOUT_DATA = dict()
+for data, target in DataLoader(testset, batch_size=1):
+    if target.item() not in HOLDOUT_DATA:
+        HOLDOUT_DATA[target.item()] = data.squeeze()
+        if len(HOLDOUT_DATA) == 10:
+            break
+HOLDOUT_DATA = t.stack([HOLDOUT_DATA[i] for i in range(10)]).to(dtype=t.float, device=device).unsqueeze(1)
+
+display_data(HOLDOUT_DATA, nrows=1, title="MNIST holdout data")
+# %%
+
+# Importing all modules you'll need, from previous solutions (you're encouraged to substitute your own implementations instead, if you want to!)
+from part2_cnns.solutions import BatchNorm2d, Conv2d, Linear, ReLU, Sequential
+
+from part5_vaes_and_gans.solutions import ConvTranspose2d
+
+
+class Autoencoder(nn.Module):
+    def __init__(self, latent_dim_size: int, hidden_dim_size: int):
+        """Creates the encoder & decoder modules."""
+        super().__init__()
+        self.hidden_dim_size = hidden_dim_size
+        self.latent_dim_size = latent_dim_size
+
+        self.encoder = Sequential(
+            Conv2d(1, 16, kernel_size=4, stride=2, padding=1),
+            ReLU(),
+            Conv2d(16, 32, kernel_size=4, stride=2, padding=1),
+            ReLU(),
+            Rearrange("b c h w -> b (h w c)"),
+            Linear(32 * 7 * 7, hidden_dim_size),
+            ReLU(),
+            Linear(hidden_dim_size, latent_dim_size),
+        )
+        self.decoder = Sequential(
+            Linear(latent_dim_size, hidden_dim_size),
+            ReLU(),
+            Linear(hidden_dim_size, 32 * 7 * 7),
+            Rearrange("b (h w c) -> b c h w", h=7, w=7, c=32),
+            ReLU(),
+            ConvTranspose2d(32, 16, kernel_size=4, stride=2, padding=1),
+            ReLU(),
+            ConvTranspose2d(16, 1, kernel_size=4, stride=2, padding=1),
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Returns the reconstruction of the input, after mapping through encoder & decoder."""
+        latent = self.encoder(x)
+        recon = self.decoder(latent)
+        return recon
+
+
+tests.test_autoencoder(Autoencoder)
+
+# %%
+
+@dataclass
+class AutoencoderArgs:
+    # architecture
+    latent_dim_size: int = 5
+    hidden_dim_size: int = 128
+
+    # data / training
+    dataset: Literal["MNIST", "CELEB"] = "MNIST"
+    batch_size: int = 512
+    epochs: int = 10
+    lr: float = 1e-3
+    betas: tuple[float, float] = (0.5, 0.999)
+
+    # logging
+    use_wandb: bool = False
+    wandb_project: str | None = "vhong-day5-autoencoder"
+    wandb_name: str | None = None
+    log_every_n_steps: int = 250
+
+
+class AutoencoderTrainer:
+    def __init__(self, args: AutoencoderArgs):
+        self.args = args
+        self.trainset = get_dataset(args.dataset)
+        self.trainloader = DataLoader(self.trainset, batch_size=args.batch_size, shuffle=True)
+        self.model = Autoencoder(
+            latent_dim_size=args.latent_dim_size,
+            hidden_dim_size=args.hidden_dim_size,
+        ).to(device)
+        self.optimizer = t.optim.Adam(self.model.parameters(), lr=args.lr, betas=args.betas)
+
+        self.step = 0
+
+
+    def training_step(self, img: Tensor) -> Tensor:
+        """
+        Performs a training step on the batch of images in `img`. Returns the loss. Logs to wandb if enabled.
+        """
+        output = self.model(img)
+        loss = t.nn.MSELoss(reduction="mean")(output, img)
+        loss.backward()
+
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+
+        self.step += 1
+
+        if self.args.use_wandb:
+            wandb.log({"loss": loss.item()}, step=self.step)
+
+        return loss
+
+    @t.inference_mode()
+    def log_samples(self) -> None:
+        """
+        Evaluates model on holdout data, either logging to weights & biases or displaying output.
+        """
+        assert self.step > 0, "First call should come after a training step. Remember to increment `self.step`."
+        output = self.model(HOLDOUT_DATA)
+        if self.args.use_wandb:
+            wandb.log({"source_images": [wandb.Image(arr) for arr in HOLDOUT_DATA.cpu().numpy()]}, step=self.step)
+            wandb.log({"output_images": [wandb.Image(arr) for arr in output.cpu().numpy()]}, step=self.step)
+        else:
+            display_data(t.concat([HOLDOUT_DATA, output]), nrows=2, title="AE reconstructions")
+
+    def train(self) -> Autoencoder:
+        """Performs a full training run."""
+        self.step = 0
+        if self.args.use_wandb:
+            wandb.init(project=self.args.wandb_project, name=self.args.wandb_name)
+            wandb.watch(self.model)
+
+        # YOUR CODE HERE - iterate over eeochs, and train your model
+        for epoch in range(self.args.epochs):
+            progress_bar = tqdm(self.trainloader, ascii=True)
+            for img, _label in progress_bar:
+                img = img.to(device)
+                loss = self.training_step(img)
+
+                progress_bar.set_description(f"{epoch=:02d}/{self.args.epochs}, {loss=:.4f}, step={self.step:05d}")
+
+                if self.step % self.args.log_every_n_steps == 0:
+                    self.log_samples()
+
+        if self.args.use_wandb:
+            wandb.finish()
+
+        return self.model
+
+
+args = AutoencoderArgs(use_wandb=True)
+trainer = AutoencoderTrainer(args)
+autoencoder = trainer.train()
+ 
+
+# %%
+
+wandb.init(project="vhong-test")
+
+# %%
+
+img, _label = next(iter(trainer.trainloader))
+print(img.shape)
+y = trainer.model(img.to(device))
+print(y.shape)
+y = (y - y.min()) / (y.max() - y.min())
+y = (y * 255).to(dtype=t.uint8)
+print(y[0].shape)
+
+wandb.log({"images": wandb.Image(y[0].cpu(), caption="Original Image")}, step=5)
